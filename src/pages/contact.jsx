c031d09b2f64bc3bbdcb69bde7ad894644328e03
Amit Sharma <mrsharma825@gmail.com>
2023-07-01 20:10:00 +0530

    inital code

---
+++ src/pages/contact.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+export const Contact = () => {
+  
+   
+  return (
+    <div className="contact-page">
+      <h1>Contact Us</h1>
+      <p>
+        You can reach out to us through the following channels:
+      </p>
+      <ul>
+        <li>Email: chance@mail.com</li>
+        <li>Phone:  +91-98765-43210</li>
+        <li>Address: Chandigarh (U.T.),INDIA</li>
+      </ul>
+    </div>
+  );
+};
